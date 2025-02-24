161b6d30a9f3e5ae8085624e870b3315cafe8f79
TinWoLiu <timmyliu123321@gmail.com>
2025-02-24 16:21:05 +0000

    finish string checking for customerRead ID

--- src/main/java/org/example/NewsAgentCreate.java
+++ src/main/java/org/example/NewsAgentCreate.java
@@ -105,7 +105,7 @@ public class NewsAgentCreate {
             System.out.println("Enter a Customer ID");
             option = input.nextInt();
             input.nextLine();
-            cr.selectCustomerMod(option);
+            cr.selectCustomerMod(String.valueOf(option));
         }
     }
 
--- src/main/java/org/example/customerRead.java
+++ src/main/java/org/example/customerRead.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 
 public class customerRead {
 
-    public boolean selectCustomerMod(int id){
+    public boolean selectCustomerMod(String id){
         try {
             // Assume selectCustomers returns a Customer object or null if not found
-            ArrayList<ArrayList<String>> customer = DBClass.selectCustomers(id);
+            ArrayList<ArrayList<String>> customer = DBClass.selectCustomers(Integer.parseInt(id));
 
             if (customer.isEmpty()) {
                 throw new IllegalArgumentException("No customer found with ID: " + id);
             } else {
                 System.out.println("Customer found: " + customer);
             }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Input must be a number");
         } catch (Exception e) {
             // Handle other exceptions (e.g., database connection issues, item no exist in DB)
             throw new IllegalArgumentException("Error occurred: " + e.getMessage());

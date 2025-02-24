a91bf134e568c3b712053b7b0a39088a174b15f2
Kishordaran Rames <kisrames@gmail.com>
2025-02-24 16:00:53 +0000

    merged zitao and cjj to dev

--- src/main/java/org/example/deliveryCreate.java
+++ src/main/java/org/example/deliveryCreate.java
@@ -37,23 +37,6 @@ public class deliveryCreate {
         this.description = description;
     }
 
-    /*public deliveryCreate(String name, String description) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Delivery area name cannot be empty");
-        }
-        if (name.length() > 255) {
-            throw new IllegalArgumentException("Invalid delivery area name, valid name is between 1-255 characters");
-        }
-        if (description == null || description.isEmpty()) {
-            throw new IllegalArgumentException("Delivery area description cannot be empty");
-        }
-        if (description.length() > 1024) {
-            throw new IllegalArgumentException("Invalid delivery area description, valid description is between 1-1024 characters");
-        }
-        this.name = name;
-        this.description = description;
-    }
-*/
     // Getter 方法
     public String getName() {
         return name;

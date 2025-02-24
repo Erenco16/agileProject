22676399322e29f0b4737d14ee11d1406b1ba343
Kishordaran Rames <kisrames@gmail.com>
2025-02-24 15:12:01 +0000

    made changes to the cli part 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         new NewsAgentCreate().mainPage();
-//        try (Connection conn = DatabaseConnection.getConnection()) {
-//            System.out.println("Connected to SQLite successfully!");
-//            new NewsAgentCreate().mainPage();
-//        } catch (SQLException e) {
-//            System.out.println("Connection failed: " + e.getMessage());
-//        }
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            System.out.println("Connected to SQLite successfully!");
+            new NewsAgentCreate().mainPage();
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
     }
 
 }

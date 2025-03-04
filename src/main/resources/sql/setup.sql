2500f604fee9e889bc02daa7c0c7d3d2f5f6a9cd
Erenco19 <erenbasaran50@gmail.com>
2025-03-04 16:28:16 +0000

    Updated the db

--- src/main/resources/sql/setup.sql
+++ src/main/resources/sql/setup.sql
@@ -39,13 +39,37 @@ description TEXT NOT NULL,
 price REAL NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS OrdersStatus (
+CREATE TABLE IF NOT EXISTS Orders (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 cust_id INTEGER NOT NULL,
 pub_id INTEGER NOT NULL,
 quantity INTEGER NOT NULL CHECK (quantity > 0),
 order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 status TEXT NOT NULL CHECK (status IN ('Pending', 'Shipped', 'Delivered', 'Cancelled')),
-FOREIGN KEY (cust_id) REFERENCES Customers(id) ON DELETE CASCADE,
-FOREIGN KEY (pub_id) REFERENCES Publication(id) ON DELETE CASCADE
+FOREIGN KEY (cust_id) REFERENCES Customers(id),
+FOREIGN KEY (pub_id) REFERENCES Publication(id)
+);
+
+-- Delivery Man Table
+CREATE TABLE IF NOT EXISTS DeliveryMan (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+employment_status TEXT NOT NULL CHECK (employment_status IN ('Active', 'Inactive'))
+);
+
+-- DeliveryDocket Table
+CREATE TABLE IF NOT EXISTS DeliveryDocket (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+date DATETIME DEFAULT CURRENT_TIMESTAMP,
+delivery_man_id INTEGER,
+docket_status TEXT NOT NULL CHECK (docket_status IN ('Completed', 'On Delivery', 'Pending', 'Cancelled')),
+FOREIGN KEY (delivery_man_id) REFERENCES DeliveryMan(id)
+);
+
+-- Invoice Table
+CREATE TABLE IF NOT EXISTS Invoice (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+order_id INTEGER NOT NULL,
+total_payable_amount REAL NOT NULL,
+FOREIGN KEY (order_id) REFERENCES OrdersStatus(id) ON DELETE CASCADE
 );

a754312bca072181b85af91c4db72df5ef717495
Kishordaran Rames <kisrames@gmail.com>
2025-03-18 14:39:39 +0000

    completed product skeleton for all entities

--- src/main/java/org/example/Invoice.java
+++ src/main/java/org/example/Invoice.java
@@ -1,4 +1,36 @@
 package org.example;
 
 public class Invoice {
+
+    public String orderid;
+    public String total_payable_amount_due;
+
+    public Invoice(String orderid, String total_payable_amount_due) {
+        this.orderid = orderid;
+        this.total_payable_amount_due = total_payable_amount_due;
+    }
+
+    public String getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(String orderid) {
+        this.orderid = orderid;
+    }
+
+    public String getTotal_payable_amount_due() {
+        return total_payable_amount_due;
+    }
+
+    public void setTotal_payable_amount_due(String total_payable_amount_due) {
+        this.total_payable_amount_due = total_payable_amount_due;
+    }
+
+    public boolean validateOrderId(){
+        return true;
+    }
+
+    public boolean validateTotalPayableAmountDue(){
+        return true;
+    }
 }
--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,4 +1,78 @@
 package org.example;
 
 public class Order {
+
+    public String custId;
+    public String pubId;
+    public String quantity;
+    public String date;
+    public String orderStatus;
+
+    public Order(String custId, String date, String orderStatus, String pubId, String quantity) {
+        this.custId = custId;
+        this.date = date;
+        this.orderStatus = orderStatus;
+        this.pubId = pubId;
+        this.quantity = quantity;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getPubId() {
+        return pubId;
+    }
+
+    public void setPubId(String pubId) {
+        this.pubId = pubId;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean validateCustId(){
+        return true;
+    }
+
+    public boolean validatePubId(){
+        return true;
+    }
+
+    public boolean validateQuantity(){
+        return true;
+    }
+
+    public boolean validateDate(){
+        return true;
+    }
+
+    public boolean validateOrderStatus(){
+        return true;
+    }
 }
